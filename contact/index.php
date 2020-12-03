90668104d78daa77efbfe7689a15a95410d8a51e
a2jaecheon <a2jaecheon@gmail.com>
2020-12-03 12:17:04 +0900

    update 2020.12.03

--- contact/index.php
+++ contact/index.php
@@ -40,10 +40,10 @@
 		}
 		return $string;
 	}
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-  
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+	<head>
+		
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta name="robots" content="index, follow" />
 		<meta name="generator" content="RapidWeaver" />
@@ -55,63 +55,38 @@
 	<meta property="og:site_name" content="CGiTEC">
 	<meta property="og:title" content="Contact | CGiTEC">
 	<meta property="og:url" content="https://cgitec.com/contact/index.php">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width">
-  <meta name="baseurl" content="https://cgitec.com/">
-
-  <title>Contact | CGiTEC</title>
-
-  <link rel="stylesheet" type="text/css" media="all" href="../rw_common/themes/offroad/consolidated.css?rwcache=628652972" />
+		<title>Contact | CGiTEC</title>
+		<link rel="stylesheet" type="text/css" media="all" href="../rw_common/themes/moderngrunge/consolidated.css?rwcache=628657927" />
 		
-  
-
-  <!--[if lt IE 9]>
-    <script src="../rw_common/themes/offroad/assets/javascript/html5shiv.js"></script>
-    <script src="../rw_common/themes/offroad/assets/javascript/respond.js"></script>
-  <![endif]-->
-
-  
-  
-  
-</head>
-
+		
+		
+		
+		
+		<!--[if IE 6]><style type="text/css" media="screen">body {behavior: url(../rw_common/themes/moderngrunge/csshover.htc);}</style><![endif]-->
+		<!--[if IE 6]><link rel="stylesheet" type="text/css" media="screen" href="../rw_common/themes/moderngrunge/ie.css" />
+		<![endif]-->
+		<script type="text/javascript" src="../rw_common/themes/moderngrunge/javascript.js?rwcache=628657927"></script>
+		
+		
+		
+	</head>
 <body>
-  <div id="page" class="site">
-    <a class="sr-only sr-only-focusable" href="#content">Skip to main content</a>
-
-    <nav class="site-navigation" role="navigation">
-      <div class="row">
-        <div class="col-xs-12">
-          <div class="site-navigation-inner">
-            <ul><li><a href="../home/" rel="">Home</a></li><li><a href="../about/" rel="">About</a></li><li><a href="../" rel="" class="parent">Products</a><ul><li><a href="../page-3/" rel="">Team</a></li><li><a href="../page-2/" rel="">Link</a></li></ul></li><li class="selected"><a href="./" rel="" class="current">Contact</a></li><li><a href="../support/" rel="">Support</a></li></ul>
-          </div>
-        </div>
-      </div>
-    </nav><!-- .site-navigation -->
-
-    <header id="masthead" class="site-header" role="banner">
-      <div class="container">
-        <div class="site-branding">
-          <div class="site-title">
-            <a href="https://cgitec.com/">CGiTEC</a>
-          </div>
-
-          <div class="site-logo">
-            <a href="https://cgitec.com/" class="site-logo"></a>
-          </div>
-
-          <div class="site-description">
-            Changing the world, one site at a time&hellip;
-          </div>
-        </div><!-- .site-branding -->
-      </div><!-- .container -->
-    </header><!-- .site-header -->
-
-    <div class="container container-main">
-      <div class="row">
-        <main id="content" class="site-content col-xs-12 col-md-9" role="main">
-          <div class="site-content-inner">
-            
+<div id="top_grad"></div>
+<div id="container"><!-- Start container -->
+	
+	<div id="pageHeader"><!-- Start page header -->
+		
+		<h1>CGiTEC</h1>
+		<h2>Changing the world, one site at a time&hellip;</h2>
+	</div><!-- End page header -->
+	
+	<div id="navcontainer"><!-- Start Navigation -->
+		<ul><li><a href="../home/" rel="">Home</a></li><li><a href="../about/" rel="">About</a></li><li><a href="../" rel="">Products</a><ul><li><a href="../page-3/" rel="">Team</a></li><li><a href="../page-2/" rel="">Link</a></li></ul></li><li><a href="./" rel="" id="current">Contact</a></li><li><a href="../support/" rel="">Support</a></li></ul>
+	</div><!-- End navigation --> 
+	
+	<div id="contentContainer"><!-- Start main content wrapper -->
+		<div id="content"><!-- Start content -->
+			
 <div class="message-text"><?php echo $_SESSION['formMessage']; unset($_SESSION['formMessage']); ?></div><br />
 
 <form class="rw-contact-form" action="./files/mailer.php" method="post" enctype="multipart/form-data">
@@ -160,37 +135,27 @@
 
 <?php unset($_SESSION['form']); ?>
 
-          </div>
-        </main>
+		</div><!-- End content -->
+	</div><!-- End main content wrapper -->
+	
+	<div id="sidebarContainer"><!-- Start Sidebar wrapper -->
+		<div id="sidebar"><!-- Start sidebar content -->
+			<h1 class="sideHeader">Sidebar</h1><!-- Sidebar header -->
+			<img class="imageStyle" alt="H100" src="files/h100.jpg" width="156" height="157" /><br /><br /><br /><span style="font:13px HelveticaNeue; ">3-D Position Sensing </span><span style="font:13px AppleSDGothicNeo-Regular; ">기술을</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">이용한</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">실시간</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">방사선</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">이미징</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">장치인</span><span style="font:13px HelveticaNeue; "> POLARIS Series </span><span style="font:13px AppleSDGothicNeo-Regular; ">검출기는</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">우수한</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">에너지</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">분해능과</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">자동화된</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">스펙트럼</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">분석으로</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">방사선의</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">종류와</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">세기를</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">정확하게</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">판별하는</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">최첨단의</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">상온반도체</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">방사선</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">검출기입니다</span><span style="font:13px HelveticaNeue; ">. </span><span style="font:13px AppleSDGothicNeo-Regular; ">볼</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">수</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">없는</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">감마선</span><span style="font:13px HelveticaNeue; "> (invisible gamma-ray)</span><span style="font:13px AppleSDGothicNeo-Regular; ">을</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">실시간으로</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">시각화하여</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">방사선</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">작업자가</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">방사선</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">위험에</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">효과적으로</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">대처할</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">수</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">있도록</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">직관적인</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">정보를</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">제공합니다</span><span style="font:13px HelveticaNeue; ">.</span><!-- sidebar content you enter in the page inspector -->
+			 <!-- sidebar content such as the blog archive links -->
+		</div><!-- End sidebar content -->
+	</div><!-- End sidebar wrapper -->
+	
+	<div class="clearer"></div>
 
-        <aside class="site-sidebar col-xs-12 col-md-3">
-          <div class="sidebar">
-            <img class="imageStyle" alt="H100" src="files/h100.jpg" width="156" height="157" /><br /><br /><br /><span style="font:13px HelveticaNeue; ">3-D Position Sensing </span><span style="font:13px AppleSDGothicNeo-Regular; ">기술을</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">이용한</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">실시간</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">방사선</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">이미징</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">장치인</span><span style="font:13px HelveticaNeue; "> POLARIS Series </span><span style="font:13px AppleSDGothicNeo-Regular; ">검출기는</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">우수한</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">에너지</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">분해능과</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">자동화된</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">스펙트럼</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">분석으로</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">방사선의</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">종류와</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">세기를</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">정확하게</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">판별하는</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">최첨단의</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">상온반도체</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">방사선</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">검출기입니다</span><span style="font:13px HelveticaNeue; ">. </span><span style="font:13px AppleSDGothicNeo-Regular; ">볼</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">수</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">없는</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">감마선</span><span style="font:13px HelveticaNeue; "> (invisible gamma-ray)</span><span style="font:13px AppleSDGothicNeo-Regular; ">을</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">실시간으로</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">시각화하여</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">방사선</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">작업자가</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">방사선</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">위험에</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">효과적으로</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">대처할</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">수</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">있도록</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">직관적인</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">정보를</span><span style="font:13px HelveticaNeue; "> </span><span style="font:13px AppleSDGothicNeo-Regular; ">제공합니다</span><span style="font:13px HelveticaNeue; ">.</span>
-          </div>
+	<div id="breadcrumbcontainer"><!-- Start the breadcrumb wrapper -->
+			
+	</div><!-- End breadcrumb -->
 
-          <div class="plugin-sidebar">
-            
-          </div>
-        </aside>
-      </div>
-    </div>
+	<div id="footer"><!-- Start Footer -->
+		<p>Copyrights &copy; 2014 CGITEC <br>씨지텍, 34187 대전광역시 유성구 계룡로141번길 29-25, (TEL: +82-42-825-1838) <br>CGITEC, 29-25, Gyeryong-ro 141beon-gil, Yuseong-gu, Daejeon 34187, Korea <a href="#" id="rw_email_contact">Contact Me</a><script type="text/javascript">var _rwObsfuscatedHref0 = "mai";var _rwObsfuscatedHref1 = "lto";var _rwObsfuscatedHref2 = ":cg";var _rwObsfuscatedHref3 = "ite";var _rwObsfuscatedHref4 = "c.i";var _rwObsfuscatedHref5 = "nfo";var _rwObsfuscatedHref6 = "@gm";var _rwObsfuscatedHref7 = "ail";var _rwObsfuscatedHref8 = ".co";var _rwObsfuscatedHref9 = "m";var _rwObsfuscatedHref = _rwObsfuscatedHref0+_rwObsfuscatedHref1+_rwObsfuscatedHref2+_rwObsfuscatedHref3+_rwObsfuscatedHref4+_rwObsfuscatedHref5+_rwObsfuscatedHref6+_rwObsfuscatedHref7+_rwObsfuscatedHref8+_rwObsfuscatedHref9; document.getElementById("rw_email_contact").href = _rwObsfuscatedHref;</script></p>
+	</div><!-- End Footer -->
 
-    <footer id="footer" class="site-footer" role="contentinfo">
-      <div class="row">
-        <div class="col-xs-12">
-          Copyrights &copy; 2014 CGITEC <br>씨지텍, 34187 대전광역시 유성구 계룡로141번길 29-25, (TEL: +82-42-825-1838) <br>CGITEC, 29-25, Gyeryong-ro 141beon-gil, Yuseong-gu, Daejeon 34187, Korea <a href="#" id="rw_email_contact">Contact Me</a><script type="text/javascript">var _rwObsfuscatedHref0 = "mai";var _rwObsfuscatedHref1 = "lto";var _rwObsfuscatedHref2 = ":cg";var _rwObsfuscatedHref3 = "ite";var _rwObsfuscatedHref4 = "c.i";var _rwObsfuscatedHref5 = "nfo";var _rwObsfuscatedHref6 = "@gm";var _rwObsfuscatedHref7 = "ail";var _rwObsfuscatedHref8 = ".co";var _rwObsfuscatedHref9 = "m";var _rwObsfuscatedHref = _rwObsfuscatedHref0+_rwObsfuscatedHref1+_rwObsfuscatedHref2+_rwObsfuscatedHref3+_rwObsfuscatedHref4+_rwObsfuscatedHref5+_rwObsfuscatedHref6+_rwObsfuscatedHref7+_rwObsfuscatedHref8+_rwObsfuscatedHref9; document.getElementById("rw_email_contact").href = _rwObsfuscatedHref;</script>
-        </div>
-      </div>
-    </footer>
-  </div>
-
-  <div class="body-overlay"></div>
-
-  <!-- Javascript includes -->
-  <script src="../rw_common/themes/offroad/javascript.js?rwcache=628652972"></script>
-  <script src="../rw_common/themes/offroad/assets/javascript/background-blur.js?rwcache=628652972"></script>
-		<script src="../rw_common/themes/offroad/assets/javascript/sidebar-right.js?rwcache=628652972"></script>
-		
-  
+</div><!-- End container -->
 </body>
 </html>
